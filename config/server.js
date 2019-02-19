5e17a6cd17adcd406f4da0cbab62f9d70f6401d7
Chris Jeffries <chrisjeffries20@chrisjeriessmbp.fritz.box>
2019-02-19 15:22:29 +0100

    second

---
+++ config/server.js
@@ -0,0 +1,6 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || 
+    '61f08499aa1b85879b111f6a8b4f7389dc0c845d3e612d22e5bcaea76868c251',
+    SECRET: process.env.SECRET || '7dc97f161a837ceb645d73f9c1cac9c5a19f3201f1cbeb2ba82ea98030356f79',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
